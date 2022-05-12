fda916221db30b70390b8263abd1fa684f5a5edc
zannatreya <zannatulbintabahar@yahoo.com>
2022-05-12 23:31:51 +0600

    using react hook form for implementing login & loading spinner also added

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -7,8 +7,8 @@ const InfoCard = ({ img, cardTitle, bgClass }) => {
                 <img className='mt-3' src={img} alt="Album" />
 
             </figure>
-            <div class="card-body text-white">
-                <h2 class="card-title">{cardTitle}</h2>
+            <div className="card-body text-white">
+                <h2 className="card-title">{cardTitle}</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
 
             </div>
--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Review = ({ review }) => {
     return (
-        <div class="card bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title">Card title!</h2>
+        <div className="card bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="flex">
-                    <div class="avatar">
-                        <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                <div className="flex">
+                    <div className="avatar">
+                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
                             <img src={review.img} alt="" />
                         </div>
                     </div>
